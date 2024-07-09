14a431ef147749c4b8aa5145b19accefb6ecb901
ptanshul <anshul.tiwari@rediffmail.com>
2024-07-09 20:09:08 +0530

    commit1

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def hello():
        print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
+       print('Request for hello1 page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
 
